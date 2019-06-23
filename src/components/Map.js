c8a438a0f9a226c0f6204ad7c9b58dc5e4e4039f
Amalia Rivera C <amaliariveracastillejos@gmail.com>
2019-06-23 17:37:12 -0500

    change library for issue with z-index

--- src/components/Map.js
+++ src/components/Map.js
@@ -1,27 +1,36 @@
 import React from 'react';
-import { Map, GoogleApiWrapper } from 'google-maps-react';
-
-const mapStyles = {
-    width: '100%',
-};
-  
-class MapContainer extends React.Component  {
-    render() {
-        return (
-            <Map
-              google={this.props.google}
-              zoom={14}
-              styles={mapStyles}
-              initialCenter={{
-               lat: -1.2884,
-               lng: 36.8233
-              }}
-            />
-          );
-    }  
-};
-
-//process.env.REACT_APP_API_KEY
-export default GoogleApiWrapper({
-    apiKey: process.env.REACT_APP_API_KEY
-  })(MapContainer);
+import GoogleMapReact from 'google-map-react';
+ 
+const AnyReactComponent = ({ text }) => <div>{text}</div>;
+ 
+class SimpleMap extends React.Component {
+  static defaultProps = {
+    center: {
+      lat: 19.390519,
+      lng: -99.4238064
+    },
+    zoom: 9
+  };
+ 
+  render() {
+    return (
+      // Important! Always set the container height explicitly
+      <div style={{ height: '100vh', width: '100%' }}>
+        <GoogleMapReact
+          bootstrapURLKeys={{ key: process.env.REACT_APP_API_KEY }}
+          defaultCenter={this.props.center}
+          defaultZoom={this.props.zoom}
+          yesIWantToUseGoogleMapApiInternals
+        >
+          <AnyReactComponent
+            lat={59.955413}
+            lng={30.337844}
+            text="My Marker"
+          />
+        </GoogleMapReact>
+      </div>
+    );
+  }
+}
+ 
+export default SimpleMap;
