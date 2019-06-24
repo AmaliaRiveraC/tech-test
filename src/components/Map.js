5c9aad8e7a3fc340cf1cd7d8ea2f29cdf8c56c79
Amalia Rivera C <amaliariveracastillejos@gmail.com>
2019-06-23 22:36:06 -0500

    changes route with dynamic values

--- src/components/Map.js
+++ src/components/Map.js
@@ -10,17 +10,26 @@ import {
   DirectionsRenderer,
 } from "react-google-maps";
 
-/*const MapWithAMarker = withScriptjs(withGoogleMap(props =>
-  <GoogleMap
-    defaultZoom={10}
-    defaultCenter={{ lat: 19.388881132451534, lng: -99.13277249999999 }}
-  >
-    <Marker
-      position={{ lat: 19.388881132451534, lng: -99.13277249999999 }}
-    />
-    {props.directions && <DirectionsRenderer directions={props.directions} />}
-  </GoogleMap>
-));*/
+
+const directions = (origin, destination, esto) => {
+  const DirectionsService = new google.maps.DirectionsService();
+  DirectionsService.route(
+    {
+      origin: new google.maps.LatLng(origin.lat, origin.lng),
+      destination: new google.maps.LatLng(destination.lat, destination.lng),
+      travelMode: google.maps.TravelMode.DRIVING
+    },
+    (result, status) => {
+      if (status === google.maps.DirectionsStatus.OK) {
+        esto.setState({
+          directions: result
+        });
+      } else {
+        console.error(`error fetching directions ${JSON.stringify(result)}`);
+      }
+    }
+  )
+ }
 const MapWithADirectionsRenderer = compose(
   withProps({
     googleMapURL: `https://maps.googleapis.com/maps/api/js?key=${process.env.REACT_APP_API_KEY}&v=3.exp&libraries=geometry,drawing,places`,
@@ -43,34 +52,26 @@ const MapWithADirectionsRenderer = compose(
         lng: this.props.destintationLongitude,
       };
 
-      const DirectionsService = new google.maps.DirectionsService();
-      DirectionsService.route(
-        {
-          origin: new google.maps.LatLng(origin.lat, origin.lng),
-          destination: new google.maps.LatLng(destination.lat, destination.lng),
-          travelMode: google.maps.TravelMode.DRIVING
-        },
-        (result, status) => {
-          if (status === google.maps.DirectionsStatus.OK) {
-            console.log(result)
-            this.setState({
-              directions: result
-            });
-          } else {
-            console.error(`error fetching directions ${JSON.stringify(result)}`);
-          }
-        }
-      )
+      directions(origin, destination, this);
      
     },
- 
+    componentDidUpdate() {
+      const origin = {
+        lat: this.props.originLatitude,
+        lng: this.props.originLongitude,
+      };
+
+      const destination = {
+        lat: this.props.destinationLatitude,
+        lng: this.props.destintationLongitude,
+      };
+
+      directions(origin, destination, this);
+    }
   })
 )(props => (
-  <GoogleMap defaultZoom={10} defaultCenter={{ lat: -34.397, lng: 150.644 }}>
-    {console.log(props)}
+  <GoogleMap defaultZoom={10} defaultCenter={{ lat: 19.37652599999999847, lng: -99.2554329999999965 }}>
     {props.directions && <DirectionsRenderer directions={props.directions} />}
   </GoogleMap>
 ));
-
-//export default MapWithAMarker;
 export default MapWithADirectionsRenderer;
