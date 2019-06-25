fabb1502b32241bb2a738c776eff70d57d56590e
Amalia Rivera C <amaliariveracastillejos@gmail.com>
2019-06-25 14:36:20 -0500

    Updates

---
+++ precache-manifest.867ba45d009abb798e8487e03b8a1a21.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "be576c5d3a29b8657a884685377f479f",
+    "url": "/tech-test/index.html"
+  },
+  {
+    "revision": "aa1d72df796bf1a39b50",
+    "url": "/tech-test/static/css/main.f1dab6fb.chunk.css"
+  },
+  {
+    "revision": "ac9c1ecb0173de15ff51",
+    "url": "/tech-test/static/js/2.64d2c384.chunk.js"
+  },
+  {
+    "revision": "aa1d72df796bf1a39b50",
+    "url": "/tech-test/static/js/main.9d982a0e.chunk.js"
+  },
+  {
+    "revision": "1020d8711361380b288b",
+    "url": "/tech-test/static/js/runtime~main.aa1a8048.js"
+  },
+  {
+    "revision": "bd158cc93c427fcdc356f309199945eb",
+    "url": "/tech-test/static/media/Grupo.bd158cc9.svg"
+  },
+  {
+    "revision": "c8d4aeabaefeaef93a6b58592dbb54bb",
+    "url": "/tech-test/static/media/Grupo1.c8d4aeab.svg"
+  },
+  {
+    "revision": "53f3e4c31def748430a4ddc0cf758815",
+    "url": "/tech-test/static/media/Grupo3.53f3e4c3.svg"
+  },
+  {
+    "revision": "f8a2477c74e6d9b33ecf8832d51fb9c6",
+    "url": "/tech-test/static/media/envelope.f8a2477c.svg"
+  },
+  {
+    "revision": "1353276c099e457a59faef9cea9325de",
+    "url": "/tech-test/static/media/logo.1353276c.svg"
+  }
+]);
